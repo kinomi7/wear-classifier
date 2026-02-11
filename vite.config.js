8e3fa36676a7b3d0dabfbc48fa2fe6442fe0c42c
kinomi7 <miyuu.niko7@gmail.com>
2026-02-12 01:12:32 +0900

    fix index.html path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()],
   base: "/wear-classifier/",
-  build: { outDir: "." }
-});
+  plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  }
+})
