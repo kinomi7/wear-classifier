b64986203467e7e886086a196fb20df6b3da4e03
kinomi7 <miyuu.niko7@gmail.com>
2026-02-25 01:59:18 +0900

    touchaction change

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function DraggableImage({ id, src, size = 160 }) {
         transform: transform
             ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
             : undefined,
-        touchAction: "none",
+        touchAction: "pan-y",
         borderRadius: 6,
         cursor: "grab"
     };
